01f0b29db8804e15e6bbdf95ed4f7fbc90c40075
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:34:38 +0100

    Create MNapomena.cs

---
+++ Projekt_Seven_Wishes/Model/MNapomena.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Seven_Wishes.Model
+{
+    class MNapomena
+    {
+       
+            public string ID { get; set; } 
+            public string Naziv { get; set; }
+            public string Opis { get; set; }
+            public DateTime Datum_dodavanja { get; set; }
+
+        }
+
+    }
+
+
+
