11f996b15f6525e6d051b64ba1bde2435ab10dcd
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:41:49 +0100

    Create Registracijacs.Designer.cs

---
+++ Projekt_Seven_Wishes/View/Registracijacs.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Projekt_Seven_Wishes
+{
+    partial class Registracijacs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registracijacs));
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtE_mail = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIme.Location = new System.Drawing.Point(448, 305);
+            this.txtIme.Multiline = true;
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(332, 32);
+            this.txtIme.TabIndex = 1;
+            this.txtIme.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPrezime.Location = new System.Drawing.Point(448, 367);
+            this.txtPrezime.Multiline = true;
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(332, 32);
+            this.txtPrezime.TabIndex = 2;
+            this.txtPrezime.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtE_mail
+            // 
+            this.txtE_mail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtE_mail.Location = new System.Drawing.Point(448, 425);
+            this.txtE_mail.Multiline = true;
+            this.txtE_mail.Name = "txtE_mail";
+            this.txtE_mail.Size = new System.Drawing.Size(332, 32);
+            this.txtE_mail.TabIndex = 3;
+            this.txtE_mail.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtLozinka.Location = new System.Drawing.Point(448, 478);
+            this.txtLozinka.Multiline = true;
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.PasswordChar = '*';
+            this.txtLozinka.Size = new System.Drawing.Size(332, 32);
+            this.txtLozinka.TabIndex = 4;
+            this.txtLozinka.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Minion Pro", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(469, 584);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 94);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Registriraj se ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnRegistracija);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Projekt_Seven_Wishes.Properties.Resources.reg;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1250, 881);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Registracijacs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 881);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtE_mail);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Registracijacs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seven Wishes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtE_mail;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+    }
+}
