2cd17e14cb6544a3cd40eddd2042975770cd7acc
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:39:01 +0100

    Create Dodavanje_rijeci.Designer.cs

---
+++ Projekt_Seven_Wishes/View/Dodavanje_rijeci.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Projekt_Seven_Wishes
+{
+    partial class Dodavanje_rijeci
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dodavanje_rijeci));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtHrvatski_naziv = new System.Windows.Forms.TextBox();
+            this.txtEngleski_naziv = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(144, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 59);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dodavanje novih riječi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tomato;
+            this.label2.Font = new System.Drawing.Font("Minion Pro SmBd", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(276, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Tomato;
+            this.label3.Font = new System.Drawing.Font("Minion Pro SmBd", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(35, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 38);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Unesi riječ na hrvatskom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Tomato;
+            this.label4.Font = new System.Drawing.Font("Minion Pro SmBd", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(35, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 38);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Unesi riječ na engleskom";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtID.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtID.ForeColor = System.Drawing.Color.White;
+            this.txtID.Location = new System.Drawing.Point(367, 207);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(271, 37);
+            this.txtID.TabIndex = 5;
+            this.txtID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtHrvatski_naziv
+            // 
+            this.txtHrvatski_naziv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtHrvatski_naziv.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtHrvatski_naziv.ForeColor = System.Drawing.Color.White;
+            this.txtHrvatski_naziv.Location = new System.Drawing.Point(367, 291);
+            this.txtHrvatski_naziv.Multiline = true;
+            this.txtHrvatski_naziv.Name = "txtHrvatski_naziv";
+            this.txtHrvatski_naziv.Size = new System.Drawing.Size(271, 37);
+            this.txtHrvatski_naziv.TabIndex = 6;
+            // 
+            // txtEngleski_naziv
+            // 
+            this.txtEngleski_naziv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtEngleski_naziv.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtEngleski_naziv.ForeColor = System.Drawing.Color.White;
+            this.txtEngleski_naziv.Location = new System.Drawing.Point(367, 379);
+            this.txtEngleski_naziv.Multiline = true;
+            this.txtEngleski_naziv.Name = "txtEngleski_naziv";
+            this.txtEngleski_naziv.Size = new System.Drawing.Size(271, 37);
+            this.txtEngleski_naziv.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Minion Pro SmBd", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(393, 531);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 58);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Odustani";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnOdustani);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Minion Pro SmBd", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(192, 531);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 58);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Spremi";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnSpremi);
+            // 
+            // Dodavanje_rijeci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projekt_Seven_Wishes.Properties.Resources.akvarij1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(727, 601);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEngleski_naziv);
+            this.Controls.Add(this.txtHrvatski_naziv);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(6)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Dodavanje_rijeci";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seven WIshes";
+            this.Load += new System.EventHandler(this.Daodavanje_rijeci_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtHrvatski_naziv;
+        private System.Windows.Forms.TextBox txtEngleski_naziv;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
