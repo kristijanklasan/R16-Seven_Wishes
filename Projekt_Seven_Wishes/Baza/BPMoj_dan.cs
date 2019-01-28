d5f26c8ffd46abd1cac3cee6458f623e6fafb998
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:34:18 +0100

    Create BPMoj_dan.cs

---
+++ Projekt_Seven_Wishes/Baza/BPMoj_dan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data; 
+
+namespace Projekt_Seven_Wishes.Baza
+{
+    class BPMoj_dan
+    {
+        public static Database database = new Database(); 
+        public static string želja = ""; 
+        public static void prikaz(Model.MMoj_dan dan)
+        {
+
+            var db = new Database().otvoridb();
+            želja = dan.Naziv_ponude;
+
+            string query = "select Naziv_želje from Lista_želja where Naziv_želje='"+želja+"'";
+            SQLiteCommand command = new SQLiteCommand(query, db);
+
+          
+            database.Close_connection(); 
+        }
+    }
+}
