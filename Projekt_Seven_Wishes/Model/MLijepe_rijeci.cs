13e3fde8277c4b5c54f4d87130a0882bf4c81483
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:34:31 +0100

    Create MLijepe_rijeci.cs

---
+++ Projekt_Seven_Wishes/Model/MLijepe_rijeci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Seven_Wishes.Model
+{
+    class MLijepe_rijeci
+    {
+        public string Engleski_naziv { get; set; }
+        public string Hrvatski_naziv { get; set; }
+        public string ID { get; set; }
+
+    }
+}
