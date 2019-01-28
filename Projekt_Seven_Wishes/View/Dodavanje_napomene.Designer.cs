254da98ad51abc4c5cd5d46cbd03c2c6a72e8a98
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:38:43 +0100

    Create Dodavanje_napomene.Designer.cs

---
+++ Projekt_Seven_Wishes/View/Dodavanje_napomene.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Projekt_Seven_Wishes
+{
+    partial class Dodavanje_napomene
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dodavanje_napomene));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateDatum = new System.Windows.Forms.DateTimePicker();
+            this.txtUnos_ID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUnos_naziva = new System.Windows.Forms.TextBox();
+            this.txtUnos_opisa = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Verdana", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(198, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(482, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dodavanje napomene";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Minion Pro SmBd", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(216, 556);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 58);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Spremi";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnUnos_napomene);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Minion Pro SmBd", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(418, 556);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 58);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Odustani";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnIzbornik);
+            // 
+            // dateDatum
+            // 
+            this.dateDatum.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.dateDatum.CalendarMonthBackground = System.Drawing.SystemColors.InfoText;
+            this.dateDatum.CalendarTrailingForeColor = System.Drawing.SystemColors.Desktop;
+            this.dateDatum.Font = new System.Drawing.Font("Cooper Black", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateDatum.Location = new System.Drawing.Point(256, 476);
+            this.dateDatum.Name = "dateDatum";
+            this.dateDatum.Size = new System.Drawing.Size(368, 34);
+            this.dateDatum.TabIndex = 11;
+            this.dateDatum.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // txtUnos_ID
+            // 
+            this.txtUnos_ID.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.txtUnos_ID.BackColor = System.Drawing.Color.Tomato;
+            this.txtUnos_ID.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUnos_ID.ForeColor = System.Drawing.Color.White;
+            this.txtUnos_ID.Location = new System.Drawing.Point(256, 124);
+            this.txtUnos_ID.Multiline = true;
+            this.txtUnos_ID.Name = "txtUnos_ID";
+            this.txtUnos_ID.Size = new System.Drawing.Size(378, 37);
+            this.txtUnos_ID.TabIndex = 12;
+            this.txtUnos_ID.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Cooper Std Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(81, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 36);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Unesi ID:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Projekt_Seven_Wishes.Properties.Resources.arena_izbornik;
+            this.pictureBox3.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(797, 625);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Cooper Std Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(38, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 36);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Unesi naziv:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Cooper Std Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(141, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 36);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Opis:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Cooper Std Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(108, 478);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 36);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Datum:";
+            // 
+            // txtUnos_naziva
+            // 
+            this.txtUnos_naziva.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.txtUnos_naziva.BackColor = System.Drawing.Color.Tomato;
+            this.txtUnos_naziva.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUnos_naziva.ForeColor = System.Drawing.Color.White;
+            this.txtUnos_naziva.Location = new System.Drawing.Point(256, 180);
+            this.txtUnos_naziva.Multiline = true;
+            this.txtUnos_naziva.Name = "txtUnos_naziva";
+            this.txtUnos_naziva.Size = new System.Drawing.Size(378, 37);
+            this.txtUnos_naziva.TabIndex = 18;
+            this.txtUnos_naziva.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // txtUnos_opisa
+            // 
+            this.txtUnos_opisa.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.txtUnos_opisa.BackColor = System.Drawing.Color.Tomato;
+            this.txtUnos_opisa.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUnos_opisa.ForeColor = System.Drawing.Color.White;
+            this.txtUnos_opisa.Location = new System.Drawing.Point(256, 266);
+            this.txtUnos_opisa.Multiline = true;
+            this.txtUnos_opisa.Name = "txtUnos_opisa";
+            this.txtUnos_opisa.Size = new System.Drawing.Size(378, 163);
+            this.txtUnos_opisa.TabIndex = 19;
+            this.txtUnos_opisa.TextChanged += new System.EventHandler(this.textBox2_TextChanged_2);
+            // 
+            // Dodavanje_napomene
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 626);
+            this.Controls.Add(this.txtUnos_opisa);
+            this.Controls.Add(this.txtUnos_naziva);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUnos_ID);
+            this.Controls.Add(this.dateDatum);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Dodavanje_napomene";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seven Wishes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateDatum;
+        private System.Windows.Forms.TextBox txtUnos_ID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUnos_naziva;
+        private System.Windows.Forms.TextBox txtUnos_opisa;
+    }
+}
