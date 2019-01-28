d02eb29034bb6b018f900e96559e892260fba05e
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:34:40 +0100

    Create MPonude.cs

---
+++ Projekt_Seven_Wishes/Model/MPonude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Seven_Wishes.Model
+{
+    class MPonude
+    {
+        public string Kategorija { get; set; }
+        public string Naziv_ponude { get; set; }
+        public string Cijena { get; set; }
+        public string Adresa { get; set; }
+        public string E_mail { get; set; }
+        public string Opis { get; set; }
+        public string Ocjena { get; set; }
+        public DateTime Datum_dodavanja { get; set; }
+        public byte[] Slika {get; set;}
+        public string ID { get; set; }
+
+    }
+ }
+
