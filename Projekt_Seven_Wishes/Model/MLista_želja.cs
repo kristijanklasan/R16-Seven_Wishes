cfc586bb6b63162340e8c109dccaa59715667340
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:34:33 +0100

    Create MLista_želja.cs

---
+++ Projekt_Seven_Wishes/Model/MLista_želja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Seven_Wishes.Model
+{
+    class MLista_želja
+    {
+        public string Naziv_želje { get; set; }
+        public string Datum_dodavanja { get; set; }
+        public string ID_ponude { get; set; }
+    }
+}
