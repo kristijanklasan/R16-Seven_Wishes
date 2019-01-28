fa1d50bead0ed9ab819ce5790a87a925c22142b7
kristijanklasan <kklasan@unipu.hr>
2019-01-28 01:39:06 +0100

    Create Form1.cs

---
+++ Projekt_Seven_Wishes/View/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Seven_Wishes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrijava(object sender, EventArgs e)
+        {
+            this.Hide(); // skrivanje prošlih prozora
+            Prijava prijava = new Prijava();  // nova instanca
+            prijava.Show(); // prikazi prozor na koji ste kliknuli
+        }
+
+        private void btnRegistriraj(object sender, EventArgs e)
+        {
+            this.Hide();  
+            Registracijacs registriraj = new Registracijacs(); 
+            registriraj.Show(); 
+
+        }
+        #region pb
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
